a4b29850885fb5af2fcffad3fd7551980851ea0b
uxvu1196 <uxvu1196@gmail.com>
2021-07-19 17:32:08 +0200

    First commit

---
+++ assets/js/Lev1_1.js
@@ -0,0 +1,18 @@
+document.write('<h1>Hello world</h1>')
+document.write('<p>Have a nice day!</p>')
+
+var myText="Hello again<br>"
+document.write(myText)
+
+var name="Uyen"
+var nachName="Vu"
+document.write("<br>Name: " + name)
+document.write("<br>Nachname: " + nachName)
+
+
+// Eine Funktion muss man mit Klammern
+// ausgeben = write - ins Dokument schreiben in meinem HTML
+// document.write("Name: " + name); // name wird durchgestrichen, weil es ein reserviertes Wort ist!!!
+// document.write("<br>Nachname: " + nachName);
+
+
